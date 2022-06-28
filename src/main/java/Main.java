0dd1d2f18cccb45b8190022bd60a07d7a6295e83
William58141 <william.fosmark@outlook.com>
2022-06-29 01:16:45 +0200

    JSON parse & JSON array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
@@ -27,7 +31,27 @@ public class Main {
 
                 StringBuilder informationString = new StringBuilder();
                 Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    informationString.append(scanner.nextLine());
+                }
+                //Close scanner
+                scanner.close();
+
+                System.out.println(informationString);
+
+                JSONParser parse = new JSONParser();
+                JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
+
+                //Get the first JSON object in the JSON array
+                System.out.println(dataObject.get(0));
+
+                JSONObject countryData = (JSONObject) dataObject.get(0);
+
+                System.out.println("location_type");
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
