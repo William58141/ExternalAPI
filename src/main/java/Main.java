289a67aa47a614226ef4fa794b78c8838047779e
William58141 <william.fosmark@outlook.com>
2022-06-29 01:01:04 +0200

    Add project

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            //Public API:
+            //https://www.metaweather.com/api/location/search/?query=<CITY>
+            //https://www.metaweather.com/api/location/<WOEID>/
+
+            URL url = new URL("https://www.metaweather.com/api/location/search/?query=London");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+
+        }
+    }
+}
